a02a52ac7317446b1d32591538fbbae62be6376b
vikas yadav <vikas626639@gmail.com>
2021-02-08 04:42:42 +0530

    hello app

---
+++ 02-hello-app/src/main/java/com/nit/rest/Hello_rest_Controller.java
@@ -0,0 +1,13 @@
+package com.nit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello_rest_Controller {
+	@GetMapping("/hello/{name}")
+	public String get_Hello(@PathVariable("name")String name) {
+		return "hello "+name;
+	}
+}
